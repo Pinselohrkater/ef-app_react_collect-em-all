c33abc48a5657115cad765e32cca182a9ad81574
Pinselohrkater <cougarism@gmail.com>
2018-07-22 09:55:15 +0200

    adding support for portrait/landscape scoreboards

--- src/components/scoreboardScreen/PlayerTable.js
+++ src/components/scoreboardScreen/PlayerTable.js
@@ -10,18 +10,30 @@ import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
     tableCellHeader: {
-        padding: '1.8vh',
-        fontSize: '2.5vh',
         backgroundColor: theme.palette.primary.main,
         color: theme.palette.getContrastText( theme.palette.primary.main ),        
     },
+    landscape_tableCellHeader: {
+        padding: '1.8vh',
+        fontSize: '2.5vh',
+    },
+    portrait_tableCellHeader: {
+        padding: '1.8vw',
+        fontSize: '2.6vw',
+    },    
     tableRow: {
         height: 'auto',
     },
     tableCell: {
-        padding: '1.8vh',
         ...theme.typography.body1,
+    },
+    landscape_tableCell: {
+        padding: '1.8vh',
         fontSize: '2.5vh'
+    },    
+    portrait_tableCell: {
+        padding: '1.8vw',
+        fontSize: '2.6vw'
     },
     primary: {
         backgroundColor: theme.palette.primary.light,
@@ -35,9 +47,14 @@ const styles = theme => ({
         textAlign: 'center'
     },
     big: {
-        fontSize: '2.5vh',
         fontWeight: 'bold'
     },
+    landscape_big: {
+        fontSize: '2.5vh',
+    },
+    portrait_big: {
+        fontSize: '2.6vw',
+    },    
     small: {
         ...theme.typography.caption,
     }
@@ -48,28 +65,31 @@ class PlayerTable extends Component {
     render() {
         const { classes } = this.props;
         const OptionalAnimation = this.props.optionalAnimation;
-        
+
+        const classPrefix = this.props.portrait ? "portrait" : "landscape";
+        const prefix = (classes, className) => `${classes[className]} ${classes[classPrefix+'_'+className]}`;
+
         return (
             <div>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell className={`${classes.tableCellHeader} ${classes.center}`} style={{ width: '5%' }}>Rank</TableCell>
-                            <TableCell className={classes.tableCellHeader} style={{ width: '75%' }}>Player</TableCell>
-                            <TableCell className={`${classes.tableCellHeader} ${classes.center}`} style={{ width: '20%' }} numeric>Catches</TableCell>
+                            <TableCell className={`${prefix(classes, 'tableCellHeader')} ${classes.center}`} style={{ width: '5%' }}>Rank</TableCell>
+                            <TableCell className={prefix(classes, 'tableCellHeader')} style={{ width: '75%' }}>Player</TableCell>
+                            <TableCell className={`${prefix(classes, 'tableCellHeader')} ${classes.center}`} style={{ width: '20%' }} numeric>Catches</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {this.props.players.map((item, key) => (
                             <OptionalAnimation animation={<Fade key={key} in={(this.props.stageProgress) > 0} timeout={{enter: key * 200}} />}>
                                 <TableRow key={key} className={classes.tableRow}>
-                                    <TableCell className={`${classes.tableCell} ${classes.center} ${classes.primary}`}>
+                                    <TableCell className={`${prefix(classes, 'tableCell')} ${classes.center} ${classes.primary}`}>
                                         <div className={classes.big}>{item.Rank}</div>
                                     </TableCell>
-                                    <TableCell className={classes.tableCell}>
+                                    <TableCell className={prefix(classes, 'tableCell')}>
                                         {item.Name}
                                     </TableCell>
-                                    <TableCell className={`${classes.tableCell} ${classes.center}`}>
+                                    <TableCell className={`${prefix(classes, 'tableCell')} ${classes.center}`}>
                                         {item.CollectionCount}
                                     </TableCell>
                                 </TableRow>
