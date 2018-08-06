dfa3113280eb9882923158f4fa18452df024f2de
Luchs <luchs@pinselohr.ch>
2018-08-06 09:42:52 +0200

    Changing API endpoint to "live"

--- src/consts/apiUrl.js
+++ src/consts/apiUrl.js
@@ -1,6 +1,6 @@
 
 export function getApiUrl() {
     return process.env.NODE_ENV === 'production' ?
-        "https://app.eurofurence.org:40000/api/v2/" :
+        "https://app.eurofurence.org/api/v2/" :
         "http://localhost:30001/api/v2/";
 }
